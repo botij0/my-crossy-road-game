ba19afc8303e118115ed8bb0b2c713621fee32b8
botij0 <javier.gonzalez.soldado@gmail.com>
2026-02-27 19:54:51 +0100

    feat: player movement

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,12 +1,23 @@
 import { Bounds } from "@react-three/drei";
+import { useRef } from "react";
+import usePlayerAnimation from "../hooks/usePlayerAnimation";
 
 export const Player = () => {
+  const player = useRef(null);
+  usePlayerAnimation(player);
+
   return (
     <Bounds fit clip observe margin={10}>
-      <mesh position={[0, 0, 10]} castShadow receiveShadow>
-        <boxGeometry args={[15, 15, 20]} />
-        <meshLambertMaterial color={0xffffff} />
-      </mesh>
+      <group ref={player}>
+        <mesh position={[0, 0, 10]} castShadow receiveShadow>
+          <boxGeometry args={[15, 15, 20]} />
+          <meshLambertMaterial color={0xffffff} flatShading />
+        </mesh>
+        <mesh position={[0, 0, 21]} castShadow receiveShadow>
+          <boxGeometry args={[2, 4, 2]} />
+          <meshLambertMaterial color={0xf0619a} flatShading />
+        </mesh>
+      </group>
     </Bounds>
   );
 }
---
+++ src/hooks/usePlayerAnimation.ts
@@ -0,0 +1,55 @@
+import { Clock, type Group, MathUtils } from "three";
+import { useFrame } from "@react-three/fiber";
+import { state, stepCompleted } from "../stores/player";
+import { tileSize } from "../constants/tiles";
+import type { RefObject } from "react";
+
+export default function usePlayerAnimation(ref: RefObject<Group | null>) {
+  const moveClock = new Clock(false);
+
+  useFrame(() => {
+    if (!ref.current) return;
+    if (!state.movesQueue.length) return;
+    const player = ref.current;
+
+    if (!moveClock.running) moveClock.start();
+
+    const stepTime = 0.2; // Seconds it takes to take a step
+    const progress = Math.min(1, moveClock.getElapsedTime() / stepTime);
+
+    setPosition(player, progress);
+    setRotation(player, progress);
+
+    // Once a step has ended
+    if (progress >= 1) {
+      stepCompleted();
+      moveClock.stop();
+    }
+  });
+}
+
+function setPosition(player: Group, progress: number) {
+  const startX = state.currentTile * tileSize;
+  const startY = state.currentRow * tileSize;
+  let endX = startX;
+  let endY = startY;
+
+  if (state.movesQueue[0] === "left") endX -= tileSize;
+  if (state.movesQueue[0] === "right") endX += tileSize;
+  if (state.movesQueue[0] === "forward") endY += tileSize;
+  if (state.movesQueue[0] === "backward") endY -= tileSize;
+
+  player.position.x = MathUtils.lerp(startX, endX, progress);
+  player.position.y = MathUtils.lerp(startY, endY, progress);
+  player.position.z = Math.sin(progress * Math.PI) * 8;
+}
+
+function setRotation(player: Group, progress: number) {
+  let endRotation = 0;
+  if (state.movesQueue[0] == "forward") endRotation = 0;
+  if (state.movesQueue[0] == "left") endRotation = Math.PI / 2;
+  if (state.movesQueue[0] == "right") endRotation = -Math.PI / 2;
+  if (state.movesQueue[0] == "backward") endRotation = Math.PI;
+
+  player.rotation.z = MathUtils.lerp(player.rotation.z, endRotation, progress);
+}
